3e39f19771e0dcc4f4a385d0fdaa0c39ee814710
Mario Bressan <mario.bressan@hotmail.com>
2018-07-08 21:15:45 -0300

    Example of protected access modifier.

---
+++ src/com/bressan/encapsulation/protectedexample/CommissionEmployee.java
@@ -0,0 +1,97 @@
+package com.bressan.encapsulation.protectedexample;
+
+// The CommissionEmployee class uses methods to manipulate its
+// private instance variables.
+public class CommissionEmployee {
+
+    protected final String firstName;
+    protected final String lastName;
+    protected final String socialSecurityNumber;
+    protected double grossSales; // vendas brutas semanais
+    protected double commissionRate; // percentagem da comissão
+
+    // construtor de cinco argumentos
+    public CommissionEmployee(String firstName, String lastName,
+                              String socialSecurityNumber, double grossSales,
+                              double commissionRate) {
+        // chamada implícita para o construtor Object ocorre aqui
+
+        // se grossSales é inválido, lança uma exceção
+        if (grossSales < 0.0) {
+            throw new IllegalArgumentException(
+                    "Gross sales must be >= 0.0");
+        }
+
+        // se commissionRate é inválido, lança uma exceção
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+            throw new IllegalArgumentException(
+                    "Commission rate must be > 0.0 and < 1.0");
+        }
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+    } // fim do construtor
+
+    // retorna o nome
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // retorna o sobrenome
+    public String getLastName() {
+        return lastName;
+    }
+
+    // retorna o número de seguro social
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    // configura a quantidade de vendas brutas
+    public void setGrossSales(double grossSales) {
+        if (grossSales < 0.0) {
+            throw new IllegalArgumentException(
+                    "Gross sales must be >= 0.0");
+        }
+
+        this.grossSales = grossSales;
+    }
+
+    // retorna a quantidade de vendas brutas
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    // configura a taxa de comissão
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+            throw new IllegalArgumentException(
+                    "Commission rate must be > 0.0 and < 1.0");
+        }
+
+        this.commissionRate = commissionRate;
+    }
+
+    // retorna a taxa de comissão
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    // calcula os lucros
+    public double earnings() {
+        return getCommissionRate() * getGrossSales();
+    }
+
+    // retorna a representação String do objeto CommissionEmployee
+    @Override
+    public String toString() {
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
+                "commission employee", getFirstName(), getLastName(),
+                "social security number", getSocialSecurityNumber(),
+                "gross sales", getGrossSales(),
+                "commission rate", getCommissionRate());
+    }
+}
