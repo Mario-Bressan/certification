acf50df1c4c1a77780857a493f4f597c48d2e6e1
Mario Bressan <mario.bressan@hotmail.com>
2018-08-01 22:55:53 -0300

    Example of reference variable and object types.

---
+++ src/com/bressan/inheritance/interfaces/p3/Office.java
@@ -0,0 +1,30 @@
+package com.bressan.inheritance.interfaces.p3;
+
+class Employee {
+    String name;
+    String address;
+    String phoneNumber;
+    float experience;
+}
+
+interface Interviewer {
+    public void conductInterview();
+}
+
+class HRExecutive extends Employee implements Interviewer {
+    String[] specialization;
+    public void conductInterview() {
+        System.out.println("HRExecutive - conducting interview");
+    }
+}
+
+public class Office {
+    public static void main(String[] args) {
+        //Interviewer hr = new HRExecutive();
+        //hr.specialization = new String[] {"Staffing"};
+        //System.out.println(hr.specialization[0]);
+        //hr.name = "Mario Bressan";
+        //System.out.println(hr.name);
+        //hr.conductInterview();
+    }
+}
