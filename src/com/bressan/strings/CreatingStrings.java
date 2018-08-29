0f2020fc6864dd998f0a724c61db51855c2c559d
Mario Bressan <mario.bressan@hotmail.com>
2018-08-28 23:28:46 -0300

    Creating Strings.

---
+++ src/com/bressan/strings/CreatingStrings.java
@@ -0,0 +1,14 @@
+package com.bressan.strings;
+
+public class CreatingStrings {
+    public static void main(String[] args) {
+
+        String s = "Mario"; // one String "Mario"
+        s.concat("Bressan"); // two Strings "Bressan" and "MarioBressan"
+        s.toLowerCase();    // new String "mario"
+        System.out.println(s); // prints Mario
+
+        //How many String objects are created?
+
+    }
+}
