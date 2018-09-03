7a3427228aca0103d955ed6c4144acc70e92fcdd
Mario Bressan <mario.bressan@hotmail.com>
2018-09-02 23:47:40 -0300

    Using static import.

---
+++ src/com/bressan/initialization/StaticImport.java
@@ -0,0 +1,12 @@
+package com.bressan.initialization;
+
+//Using static imports we are allowed to import
+//all static members of a class
+import static java.lang.System.*;
+
+public class StaticImport {
+    public static void main(String[] args) {
+        out.print("Hello!");
+
+    }
+}
